1052cbca54801551df6d4428d45368b7badf8fad
hanakirana25 <hanakiran36@gmail.com>
2020-03-30 09:55:49 +0700

    linking 2 activities

---
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnSecondActivity = findViewById(R.id.btn_second_activity);
+
+        btnSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
